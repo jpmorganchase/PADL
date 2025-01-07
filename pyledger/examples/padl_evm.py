a66c07a077ae8d81c4b78be69a5eb8cdbf47b303
shaltiel eloul <shaltieleloul@shaltiels-MacBook-Pro.local>
2025-01-07 11:03:06 +0000

    fix contract name

--- pyledger/examples/padl_evm.py
+++ pyledger/examples/padl_evm.py
@@ -7,8 +7,8 @@ in order to run this example. Two services need to be up.
 1. run: python cloud_server/python_storage/serv.py
 (this service is a simple storage for the proofs and archive of the transactions)
 2. run an evm node. For simplicity, you can use ganache-cli with the following launch command line: 
-ganache-cli --account='0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63,100000000000000000000000' --account='0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be64,100000000000000000000000' --gasLimit=9999999999999999999999999 --chain.
-vmErrorsOnRPCResponse=true --chain.allowUnlimitedContractSize
+ganache-cli --miner.blockGasLimit 10000000000000000 --miner.callGasLimit 1000000000000000
+--account="0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63,10000000000000000000000000"
 """
 from eth_account import Account
 import os
